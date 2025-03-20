87c7ecef7f3a268d46f6b4a24da9cf146006a478
polecoder <maurop4502@gmail.com>
2025-03-20 18:47:47 -0300

    primer commit libro

---
+++ include/Libro.h
@@ -0,0 +1,34 @@
+// Definicion clase Libro
+
+#ifndef LIBRO
+#define LIBRO
+
+#include <set>
+#include <string>
+
+/* #include "../include/Publicacion.h" */
+
+using namespace std;
+
+class Libro/*: public Publicacion*/ {
+  private:
+    string editorial;
+    set<string> palabrasDestacadas;
+  public:
+    Libro(string editorial, set<string> palabrasDestacadas);
+
+    // setters
+    void setEditorial(string editorial);
+
+    void agregarPalabraDestacada(string palabra);
+    void eliminarPalabraDestacada(string palabra);
+
+    // getters
+    string getEditorial();
+    set<string> getPalabrasDestacadas();
+
+    // metodos especificos
+    virtual bool contienePalabra(string palabra);
+};
+
+#endif
--- src/Libro.cpp
+++ src/Libro.cpp
@@ -1,1 +1,13 @@
-// Clase Libro
+// Implementacion clase Libro
+
+#include <set>
+#include <string>
+
+#include "../include/Libro.h"
+
+Libro::Libro(string editorial, set<string> palabrasDestacadas) {
+  this->editorial = editorial;
+  this->palabrasDestacadas = palabrasDestacadas;
+}
+
+using namespace std;
