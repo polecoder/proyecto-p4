f50fe55eb8e5156fc47d602dd6765465d4049f5c
Mauro <96805572+polecoder@users.noreply.github.com>
2025-03-21 19:36:13 -0300

    Update articuloRevista.cpp

--- src/articuloRevista.cpp
+++ src/articuloRevista.cpp
@@ -1,35 +1,34 @@
 #include "../include/DTFecha.h"
-#include "../include/DTRefer.h"
 #include "../include/Publicacion.h"
 #include "../include/Investigador.h"
 #include "../include/articuloRevista.h"
+
 #include <set>
-#include <string> 
+#include <string>
+
 using namespace std;
 
-articuloRevista ::articuloRevista(string DOI, string titulo, DTFecha fecha,set<Investigador*> autores, string nombreRevista, int volumen, int numero, DTFecha fechaPublicacion):Publicacion(DOI,titulo,fecha,autores){
+articuloRevista::articuloRevista(string DOI, string titulo, DTFecha fecha,set<Investigador*> autores, string nombreRevista, string extracto): Publicacion(DOI,titulo,fecha,autores) {
     this->revista=nombreRevista;
-    this->volumen=volumen;
-    this->numero=numero;
-    this->fechaPublicacion=fechaPublicacion;
-}
-
-void articuloRevista::setRevista(string revista){
-    this->revista=revista;
-}
-
-void articuloRevista::setExtracto(string extracto){
     this->extracto=extracto;
 }
 
-string articuloRevista::getRevista(){
+void articuloRevista::setRevista(string revista) {
+    this->revista=revista;
+}
+
+void articuloRevista::setExtracto(string extracto) {
+    this->extracto=extracto;
+}
+
+string articuloRevista::getRevista() {
     return this->revista;
 }
 
-string articuloRevista::getExtracto(){
+string articuloRevista::getExtracto() {
     return this->extracto;
 }
 
-bool articuloRevista::contienePalabra(string palabra){
-    return (this->extracto.find(palabra)!=string::npos);
+bool articuloRevista::contienePalabra(string palabra) {
+    return (this->extracto.find(palabra) != string::npos);
 }
