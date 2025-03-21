66b7b8abddf53c5018b91bc9a329782502499178
Mcb-2801 <mcastera10@gmail.com>
2025-03-21 19:42:04 -0300

    Update PaginaWeb.h
    Ahora si creo que version final

--- include/PaginaWeb.h
+++ include/PaginaWeb.h
@@ -2,23 +2,25 @@
 #define PAGINAWEB
 
 #include <string>
+#include <set>
 #include "../include/Publicacion.h"
+#include "../include/Investigador.h"
+#include "../include/DTFecha.h"
 
 using namespace std;
 
 class PaginaWeb : public PUBLICACION{
 private:
     string url;
-    string contenidoExtraido;
+    set<string> contenidoExtraido;
 public:
-    PaginaWeb(string url,string contenidoExtraido);
-    // ~PaginaWeb();
+    PaginaWeb(string DOI, string titulo, DTFecha fecha, set<investigador*> autores, string url, set<string> contenidoExtraido);
     
     void setUrl(string url);
     void setContenidoExtraido(string contenidoExtraido);
     
     string getUrl();
-    string getContenidoExtraido();
+    set<string> getContenidoExtraido();
     
     virtual bool contienePalabra(string palabra);
 };
