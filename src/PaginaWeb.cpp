963212a97467b5f453c1b5d1d2673cb65e9fb020
Mcb-2801 <mcastera10@gmail.com>
2025-03-21 19:08:05 -0300

    Update PaginaWeb.cpp
    Paginaweb version final

--- src/PaginaWeb.cpp
+++ src/PaginaWeb.cpp
@@ -1,21 +1,30 @@
+#include <set>
 #include <string>
-#include "../include/PaginaWeb.h"
+
+#include "../include/paginaWeb.h"
+#include "../include/publicacion.h"
 
 using namespace std;
 
-PaginaWeb::PaginaWeb(string url, string contenidoExtraido){
-    this->url = url;
-    this->contenidoExtraido = contenidoExtraido;
-}
 
-string PaginaWeb:: getUrl(){
-    return this->url;
-}
+PaginaWeb :: PaginaWeb(string DOI, string titulo, DTFecha fecha, set<investigador*> autores, string url, set<string> contenidoExtraido) : Publicacion(DOI, titulo, fecha, autores) url(url), contenidoExtraido(contenidoExtraido){}
 
-string PaginaWeb:: getContenidoExtraido(){
-    return this->contenidoExtraido;
-};
+    void Paginaweb:: setUrl(string url){
+        this->url = url;
+    }
 
-bool contienePalabra::contienePalabra(string palabra){
+    void PaginaWeb:: setContenidoExtraido(set<string> contenidoExtraido){
+        this->contenidoExtraido = contenidoExtraido;
+    }
 
-}
+    string PaginaWeb:: getUrl(){
+        return this->url;
+    }
+    
+    set<string> PaginaWeb:: getContenidoExtraido(){
+        return this->contenidoExtraido;
+    }
+    
+    bool contienePalabra(string palabra){
+        return (this->extracto.find(palabra)!=string::npos);
+    }
