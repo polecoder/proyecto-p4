772a1da6f6355597f3f9d0b9e03d9d96b958f5bd
Andres Blanco <andr3sblanco@gmail.com>
2025-03-25 11:06:22 -0300

    .

--- ejemplo_main_2025.cpp
+++ ejemplo_main_2025.cpp
@@ -116,14 +116,35 @@ void parte_g() {
     pub2->agregarAutor(alberto);
 }
 
-void parte_h(){
-	lis
-}
 
-void parte_i(){
+	void parte_h() {
+		Investigador* carla = coleccion_getInvestigador("0000-0003-1234-5678");
+		std::set<std::string> publicaciones = carla->listarPublicaciones(DTFecha(10,12,2023), "UML");
+		// Imprimir cada DOI en una línea
+		for (const std::string& doi : publicaciones) {
+			std::cout << doi << std::endl;
+		}
+	}
+
+void parte_i() { // Eliminar publicacion
+    std::string doi = "10.1234/abc123";
+    for (auto it = publicaciones.begin(); it != publicaciones.end(); ++it) {
+        if ((*it)->getDOI() == doi) { // Desreferenciar el puntero para acceder a getDOI()
+            publicaciones.erase(it);  // Se elimina del list
+            return;
+        }
+    }
 }
 
 void parte_j(){
+	void parte_h() {
+		Investigador* carla = coleccion_getInvestigador("0000-0003-1234-5678");
+		std::set<std::string> publicaciones = carla->listarPublicaciones(DTFecha(1,1,2020), "UML");
+		// Imprimir cada DOI en una línea
+		for (const std::string& doi : publicaciones) {
+			std::cout << doi << std::endl;
+		}
+	}
 }
 
 void parte_k(){
